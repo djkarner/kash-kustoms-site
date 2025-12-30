2c985ae1bb0d099b19949e91915dcc047a24a93b
djkarner <djkarner7@gmail.com>
2025-12-29 22:59:43 -0600

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ background: "#000", color: "#fff", minHeight: "100vh", padding: "40px" }}>
+      <h1>Kash Kustoms Tinting</h1>
+      <p>Auto • Residential • Commercial • Marine</p>
+    </main>
+  );
+}
