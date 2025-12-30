e2b665d946bf2afce88c7bcd4c73a496d00667c9
djkarner <djkarner7@gmail.com>
2025-12-29 23:47:54 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -6,9 +6,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, backgroundColor: "#000", color: "#fff" }}>
+      <body
+        style={{
+          margin: 0,
+          backgroundColor: "#000",
+          color: "#fff",
+          fontFamily: "Arial, Helvetica, sans-serif",
+        }}
+      >
         {children}
       </body>
     </html>
   );
 }
+
